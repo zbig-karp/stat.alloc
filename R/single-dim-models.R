90e2983eba4486f855c7b574ee18f4e6d02d5a0b
zbig-karp <zbigniew.karpinski@startmail.com>
2024-09-13 14:15:15 +0200

    An update to the R code

--- R/single-dim-models.R
+++ R/single-dim-models.R
@@ -1,8 +1,8 @@
-# Constant mixing model with the estimation routine which minimises the distance between observed and model-predicted status allocations. `cmm` stands for "constant mixing model", while `mde` stands for "minimum distance estimation".
-
-#' Title
+#' Constant mixing model with the minimum-distance estimation routine
 #'
-#' @param dat an object returned by stat.alloc::mar(), or a list with the following three elements: the observed status allocation, the meritocratic status allocation, and the lottery allocation.
+#' The function estimates a mixing coefficient in the constant mixing model by minimising the distance between observed and model-predicted status allocations. `cmm` stands for "constant mixing model", while `mde` stands for "minimum distance estimation".
+#'
+#' @param dat an object returned by `stat.alloc::mar()`, or a list with the following three elements: the observed status allocation, the meritocratic status allocation, and the lottery allocation.
 #'
 #' @return a list of three elements:
 #' - a data frame with the estimated mixing coefficient, its standard error and test statistics
@@ -36,11 +36,249 @@ cmm_mde <- function(dat) {
     `z test` = Estimate/`S.E.`,
     `p value` = cut(pnorm(`z test`, lower.tail = FALSE),
                     breaks = c(-Inf, 0.001, 0.01, 0.05, 0.1, Inf),
-                    labels = c("< 0.001", "< 0.0$", "< 0.05", "< 0.1", "n.s.")))
+                    labels = c("< 0.001", "< 0.01", "< 0.05", "< 0.1", "n.s.")))
 
   aprime <- out$par * dat$Meritocratic + (1 - out$par) * dat$Lottery
 
   list(`Mixing coefficient` = as.data.frame(est_table),
+       `Model-predicted allocation` = aprime,
+       `Dissimilarity index` = sum(abs(prop.table(dat$Actual) - prop.table(aprime)))/2)
+}
+
+#' Constant mixing model with maximum-likelihood estimation routine
+#'
+#' The function estimates a mixing coefficient in the constant mixing model by means of maximum likelihood. `cmm` stands for "constant mixing model", while `mde` stands for "minimum distance estimation".
+#'
+#' @param dat an object returned by `stat.alloc::mar()`, or a list with the following three elements: the observed status allocation, the meritocratic status allocation, and the lottery allocation.
+#'
+#' @return a list of three elements:
+#' - a data frame with the estimated mixing coefficient, its standard error and test statistics
+#' - a matrix representing the model-predicted status allocation table
+#' - the index of dissimilarity between observed and model-predicted status allocation tables. It is used as the model's goodness of fit measure.
+#' @export
+#'
+#' @examples
+#' ks1985 <- matrix(c(135, 52, 58, 12, 6, 27, 56, 126, 26, 7, 13, 51, 183, 98, 65, 3, 8, 30, 23, 2), ncol = 4, dimnames = list(rownames = paste0("E", 1:5), colnames = paste0("O", 1:4)))
+#' ks1985_mar <- mar(x = ks1985)
+#' cmm_mle(ks1985_mar)
+
+cmm_mle <- function(dat) {
+
+  # Function which returns the log-likelihood for the compound matrix
+  mc <- function(dat, alpha) {
+    aprime <- (alpha * dat$Meritocratic + (1 - alpha) * dat$Lottery)/sum(dat$Actual)
+
+    fake <- 99999
+    ll <- -sum(dat$Actual * log(aprime))
+
+    if (is.finite(ll)) {
+      fake <- ll
+    } else {
+      ll <- fake
+    }
+    ll
+  }
+
+  # Maximising the likelihood with respect to the mixing coefficient
+  out <- optim(par = 0.5,
+               fn = mc,
+               dat = dat,
+               method = "L-BFGS-B", lower = 0, upper = 1,
+               hessian = TRUE)
+
+  est_table <- tibble::tibble(
+    Estimate = out$par,
+    `S.E.` = sqrt(1/out$hessian[1, 1]),
+    `z test` = Estimate/`S.E.`,
+    `p value` = cut(pnorm(`z test`, lower.tail = F),
+                    breaks = c(-Inf, 0.001, 0.01, 0.05, 0.1, Inf),
+                    labels = c("< 0.001", "< 0.01", "< 0.05", "< 0.1", "n.s."))
+  )
+
+  aprime <- out$par * dat$Meritocratic + (1 - out$par) * dat$Lottery
+
+  list(`Mixing coefficient` = as.data.frame(est_table),
+       `Model-predicted allocation` = aprime,
+       `Dissimilarity index` = sum(abs(prop.table(dat$Actual) - prop.table(aprime)))/2)
+}
+
+#' Differential mixing model with the minimum-distance estimation routine
+#'
+#' The function estimates mixing coefficients in the differential mixing model by minimising the distance between observed and model-predicted status allocations. `dmm` stands for "differential mixing model", while `mde` stands for "minimum distance estimation".
+#'
+#' @param dat an object returned by `stat.alloc::mar()`, or a list with the following three elements: the observed status allocation, the meritocratic status allocation, and the lottery allocation.
+#'
+#' @return a list of four elements:
+#' - a data frame with the estimated mixing coefficients, one coefficient per origin category, their standard error and associated test statistics
+#' - a named vector of **adjustment proportions**, or conditional probabilities that an individual ends up in destination status \eqn{k} **given that** the allocation is driven by lottery. Adjustment proportions are necessary to fit column marginals of the status allocation matrix
+#' - a matrix representing the model-predicted status allocation table
+#' - the index of dissimilarity between observed and model-predicted status allocation tables. It is used as the model's goodness of fit measure.
+#' @export
+#'
+#' @examples
+#' ks1985 <- matrix(c(135, 52, 58, 12, 6, 27, 56, 126, 26, 7, 13, 51, 183, 98, 65, 3, 8, 30, 23, 2), ncol = 4, dimnames = list(rownames = paste0("E", 1:5), colnames = paste0("O", 1:4)))
+#' ks1985_mar <- mar(x = ks1985)
+#' dmm_mde(ks1985_mar)
+
+dmm_mde <- function(dat) {
+
+  # Function which returns the distance between the observed and the model-predicted allocations
+  mc <- function(dat, alpha) {
+    # Row and column marginals
+    row_mrg <- rowSums(dat$Actual)
+    col_mrg <- colSums(dat$Actual)
+
+    # "Target" proportions
+    target <- vector(mode = "double", length = length(col_mrg))
+    for (i in 1:length(target)) {
+      target[i] <- (col_mrg[i] - sum(alpha * dat$Meritocratic[, i]))/sum((1 - alpha) * row_mrg)
+    }
+
+    # "Compound" allocation
+    aprime <- dat$Actual
+    for (i in 1:nrow(aprime)) {
+      for (j in 1:ncol(aprime))
+        aprime[i, j] <- (alpha[i] * dat$Meritocratic[i, j] + (1 - alpha[i]) * target[j] * row_mrg[i])
+    }
+
+    fake <- 99999
+    ll <- norm(x = dat$Actual - aprime, type = "F")^2
+
+    if (is.finite(ll)) {
+      fake <- ll
+    } else {
+      ll <- fake
+    }
+    ll
+  }
+
+  # Minimising the the distance between the observed and the model-predicted allocations
+  out <- optim(par = rep(0.9, times = nrow(dat$Actual)),
+               fn = mc,
+               dat = dat,
+               method = "L-BFGS-B",
+               lower = rep(0, times = nrow(dat$Actual)),
+               upper = rep(1, times = nrow(dat$Actual)),
+               hessian = TRUE)
+
+  est_table <- tibble::tibble(
+    Estimate = out$par,
+    `S.E.` =  ifelse(diag(out$hessian) <= 0, NA, sqrt(diag(solve(out$hessian)))),
+    `z test` = Estimate/`S.E.`,
+    `p value` = cut(pnorm(`z test`, lower.tail = F),
+                    breaks = c(-Inf, 0.001, 0.01, 0.05, 0.1, Inf),
+                    labels = c("< 0.001", "< 0.01", "< 0.05", "< 0.1", "n.s.")))
+
+  # Calculating expected counts
+  row_mrg <- rowSums(dat$Actual)
+  col_mrg <- colSums(dat$Actual)
+  alpha <- est_table$Estimate
+
+  target <- vector(mode = "double", length = length(col_mrg))
+  for (i in 1:length(target)) {
+    target[i] <- (col_mrg[i] - sum(alpha * dat$Meritocratic[, i]))/sum((1 - alpha) * row_mrg)
+  }
+  names(target) <- colnames(dat[[1]])
+
+  aprime <- dat$Actual
+  for (i in 1:nrow(aprime)) {
+    for (j in 1:ncol(aprime))
+      aprime[i, j] <- alpha[i] * dat$Meritocratic[i, j] + (1 - alpha[i]) * target[j] * row_mrg[i]
+  }
+
+  list(`Mixing coefficient` = as.data.frame(est_table),
+       `Adjustment proportions` = target,
+       `Model-predicted allocation` = aprime,
+       `Dissimilarity index` = sum(abs(prop.table(dat$Actual) - prop.table(aprime)))/2)
+}
+
+#' Differential mixing model with the maximum-likelihood estimation routine
+#'
+#' The function estimates mixing coefficients in the differential mixing model by means of maximum likelihood. `dmm` stands for "differential mixing model", while `mle` stands for "maximum likelihood estimation".
+#'
+#' @param dat an object returned by `stat.alloc::mar()`, or a list with the following three elements: the observed status allocation, the meritocratic status allocation, and the lottery allocation.
+#'
+#' @return a list of four elements:
+#' - a data frame with the estimated mixing coefficients, one coefficient per origin category, their standard error and associated test statistics
+#' - a named vector of **adjustment proportions**, or conditional probabilities that an individual ends up in destination status \eqn{k} **given that** the allocation is driven by lottery. Adjustment proportions are necessary to fit column marginals of the status allocation matrix
+#' - a matrix representing the model-predicted status allocation table
+#' - the index of dissimilarity between observed and model-predicted status allocation tables. It is used as the model's goodness of fit measure.
+#' @export
+#'
+#' @examples
+#' ks1985 <- matrix(c(135, 52, 58, 12, 6, 27, 56, 126, 26, 7, 13, 51, 183, 98, 65, 3, 8, 30, 23, 2), ncol = 4, dimnames = list(rownames = paste0("E", 1:5), colnames = paste0("O", 1:4)))
+#' ks1985_mar <- mar(x = ks1985)
+#' dmm_mle(ks1985_mar)
+
+dmm_mle <- function(dat) {
+
+  # Function which returns the log-likelihood for the compound matrix
+  mc <- function(dat, alpha) {
+    # Row and column marginals
+    row_mrg <- rowSums(dat$Actual)
+    col_mrg <- colSums(dat$Actual)
+
+    # "Target" proportions
+    target <- vector(mode = "double", length = length(col_mrg))
+    for (i in 1:length(target)) {
+      target[i] <- (col_mrg[i] - sum(alpha * dat$Meritocratic[, i]))/sum((1 - alpha) * row_mrg)
+    }
+
+    # "Compound" allocation
+    aprime <- dat$Actual
+    for (i in 1:nrow(aprime)) {
+      for (j in 1:ncol(aprime))
+        aprime[i, j] <- (alpha[i] * dat$Meritocratic[i, j] + (1 - alpha[i]) * target[j] * row_mrg[i])/sum(dat$Actual)
+    }
+
+    fake <- 99999
+    ll <- -sum(dat$Actual * log(aprime))
+
+    if (is.finite(ll)) {
+      fake <- ll
+    } else {
+      ll <- fake
+    }
+    ll
+  }
+
+  # Maximising the likelihood with respect to the mixing coefficient
+  out <- optim(par = rep(0.5, times = nrow(dat$Actual)),
+               fn = mc,
+               dat = dat,
+               method = "L-BFGS-B",
+               lower = rep(0, times = nrow(dat$Actual)),
+               upper = rep(1, times = nrow(dat$Actual)),
+               hessian = TRUE)
+
+  est_table <- tibble::tibble(
+    Estimate = out$par,
+    `S.E.` =  ifelse(diag(out$hessian) <= 0, NA, sqrt(diag(solve(out$hessian)))),
+    `z test` = Estimate/`S.E.`,
+    `p value` = cut(pnorm(`z test`, lower.tail = FALSE),
+                    breaks = c(-Inf, 0.001, 0.01, 0.05, 0.1, Inf),
+                    labels = c("< 0.001", "< 0.01", "< 0.05", "< 0.1", "n.s."))
+  )
+
+  # Calculating expected counts
+  row_mrg <- rowSums(dat$Actual)
+  col_mrg <- colSums(dat$Actual)
+  alpha <- est_table$Estimate
+
+  target <- vector(mode = "double", length = length(col_mrg))
+  for (i in 1:length(target)) {
+    target[i] <- (col_mrg[i] - sum(alpha * dat$Meritocratic[, i]))/sum((1 - alpha) * row_mrg)
+  }
+  names(target) <- colnames(dat[[1]])
+
+  aprime <- dat$Actual
+  for (i in 1:nrow(aprime)) {
+    for (j in 1:ncol(aprime))
+      aprime[i, j] <- alpha[i] * dat$Meritocratic[i, j] + (1 - alpha[i]) * target[j] * row_mrg[i]
+  }
+
+  list(`Mixing coefficient` = as.data.frame(est_table),
+       `Adjustment proportions` = target,
        `Model-predicted allocation` = aprime,
        `Dissimilarity index` = sum(abs(prop.table(dat$Actual) - prop.table(aprime)))/2)
 }
--- README.md
+++ README.md
@@ -74,13 +74,15 @@ mar(ks1985)
 #> 
 #> $Lottery
 #>         colnames
-#> rownames      O1     O2     O3     O4
-#>       E1  46.814 43.076  72.98 15.130
-#>       E2  43.921 40.414  68.47 14.195
-#>       E3 104.411 96.074 162.77 33.745
-#>       E4  41.817 38.478  65.19 13.515
-#>       E5  26.037 23.958  40.59  8.415
+#> rownames  O1 O2  O3   O4
+#>       E1  47 43  73 15.1
+#>       E2  44 40  68 14.2
+#>       E3 104 96 163 33.7
+#>       E4  42 38  65 13.5
+#>       E5  26 24  41  8.4
 ```
+
+## Constant mixing models
 
 Function `cmm_mde()` estimates a model which treats the observed status
 allocation as a mixture of two “pure” status allocation. It returns a
@@ -94,20 +96,136 @@ list of three elements:
 ``` r
 cmm_mde(dat = mar(ks1985))
 #> $`Mixing coefficient`
-#>    Estimate        S.E.   z test p value
-#> 1 0.4476211 0.002588898 172.9002 < 0.001
+#>   Estimate   S.E. z test p value
+#> 1     0.45 0.0026    173 < 0.001
 #> 
 #> $`Model-predicted allocation`
 #>         colnames
-#> rownames        O1        O2        O3        O4
-#>       E1 105.53562  23.79427  40.31261  8.357493
-#>       E2  62.30883  59.02877  37.82138  7.841019
-#>       E3  57.67443 124.68863 195.99691 18.640026
-#>       E4  23.09883  21.25444 107.18133  7.465401
-#>       E5  14.38229  13.23389  28.68776 42.696061
+#> rownames  O1  O2  O3   O4
+#>       E1 106  24  40  8.4
+#>       E2  62  59  38  7.8
+#>       E3  58 125 196 18.6
+#>       E4  23  21 107  7.5
+#>       E5  14  13  29 42.7
 #> 
 #> $`Dissimilarity index`
-#> [1] 0.115597
+#> [1] 0.12
+```
+
+`cmm_mde()` estimates the mixing coefficient by searching for a value of
+the coefficient which minimises the distance (or the Frobenius norm of
+the difference) between the observed and model-predicted status
+allocations. An alternative, a function called `cmm_mle`, estimates the
+coefficient by means of maximum likelihood.
+
+``` r
+cmm_mle(dat = mar(ks1985))
+#> $`Mixing coefficient`
+#>   Estimate  S.E. z test p value
+#> 1     0.42 0.027     15 < 0.001
+#> 
+#> $`Model-predicted allocation`
+#>         colnames
+#> rownames  O1  O2  O3   O4
+#>       E1 102  25  42  8.8
+#>       E2  61  58  40  8.3
+#>       E3  61 123 194 19.6
+#>       E4  24  22 104  7.9
+#>       E5  15  14  29 40.5
+#> 
+#> $`Dissimilarity index`
+#> [1] 0.11
+```
+
+As we can see, the maximum-likelihood estimate is slightly lower than
+the minimum-distance one and it has a considerably larger standard
+error. In terms of fit, as measured with the dissimilarity index, the
+two estimations are nearly identical, but their predicted counts differ
+slightly. The index of dissimilarity between the two model-predicted
+allocations is:
+
+``` r
+pred_mde <- prop.table(cmm_mde(dat = mar(ks1985))[[2]])
+pred_mle <- prop.table(cmm_mle(dat = mar(ks1985))[[2]])
+sum(abs(pred_mde - pred_mle))/2
+#> [1] 0.015
+```
+
+Thus, 15 cases (individuals) out of 1,000 have to be re-allocated to
+equalise the two matrices. It’s not much in absolute terms, but it still
+shows that the two predictions differ.
+
+## Differential mixing models
+
+So far, the mixing coefficient was assumed to be constant across all
+origin categories. It is possible to relax this assumption and allow
+each origin category to have its own mixing coefficient. Function
+`dmm_mde()` fits a **differential mixing model** using the minimum
+distance estimation routine:
+
+``` r
+dmm_mde(dat = mar(ks1985))
+#> $`Mixing coefficient`
+#>   Estimate   S.E. z test p value
+#> 1     0.71 0.0039    183 < 0.001
+#> 2     0.33 0.0070     47 < 0.001
+#> 3     0.15 0.0132     12 < 0.001
+#> 4     0.34 0.0071     48 < 0.001
+#> 5     0.14 0.0083     16 < 0.001
+#> 
+#> $`Adjustment proportions`
+#>   O1   O2   O3   O4 
+#> 0.16 0.28 0.46 0.11 
+#> 
+#> $`Model-predicted allocation`
+#>         colnames
+#> rownames  O1  O2  O3   O4
+#>       E1 134  14  24  5.5
+#>       E2  46  58  52 11.9
+#>       E3  53 117 191 35.8
+#>       E4  17  29 102 11.1
+#>       E5  13  24  41 20.6
+#> 
+#> $`Dissimilarity index`
+#> [1] 0.069
+```
+
+The object returned by `dmm_mde` has a structure similar to those
+returned by `cmm_mde` or `cmm_mle`. A primary difference is that
+`dmm_mde` returns a list of four, rather than three, elements, the
+additional element being a named numeric vector of so-called adjustment
+proportions, or a special type of parameter introduced into the model to
+fit the column marginals (see Karpiński and Skvoretz 2023 for more
+details concerning the adjustment proportions).
+
+There is also a function which fits the differential model by means of
+maximum likelihood estimation, `dmm_mle()`.
+
+``` r
+dmm_mle(dat = mar(ks1985))
+#> $`Mixing coefficient`
+#>   Estimate  S.E. z test p value
+#> 1    0.723 0.035  20.94 < 0.001
+#> 2    0.413 0.063   6.55 < 0.001
+#> 3    0.094 0.132   0.71    n.s.
+#> 4    0.251 0.083   3.03  < 0.01
+#> 5    0.171 0.058   2.97  < 0.01
+#> 
+#> $`Adjustment proportions`
+#>    O1    O2    O3    O4 
+#> 0.140 0.273 0.488 0.099 
+#> 
+#> $`Model-predicted allocation`
+#>         colnames
+#> rownames  O1  O2  O3   O4
+#>       E1 136  13  24  4.9
+#>       E2  49  61  48  9.8
+#>       E3  50 113 198 35.8
+#>       E4  17  32  98 11.9
+#>       E5  11  22  42 22.7
+#> 
+#> $`Dissimilarity index`
+#> [1] 0.074
 ```
 
 # References
